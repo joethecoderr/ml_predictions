0fc99b9d665333a21aef10e8e7271558b63a71ac
Joel Gaspar <joel.e.gaspar@gmail.com>
2021-01-28 23:21:47 -0700

    renaming app server

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from utils import Utils
-from models import Models
+import joblib
+import numpy as np
 
-if __name__ == '__main__':
-    utils = Utils()
-    models = Models()
-    ds = utils.load_from_csv('datasets/felicidad.csv')
-    X, y = utils.features_target(ds, ['score','rank', 'country'],['score'])
-    models.grid_training(X,y)
-    print(ds)
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    model = joblib.load('./models/best_model.pkl')
+    X_test = np.array([7.594444821,7.479555538,1.616463184,1.53352356,0.796666503,0.635422587,0.362012237,0.315963835,2.277026653])
+    prediction = model.predict(X_test.reshape(1,-1))
+    return jsonify({'Happiness score prediction' : list(prediction)})
+
+if __name__ == "__main__":
+    
+    app.run()
