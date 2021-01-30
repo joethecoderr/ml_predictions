4c35d214175990d9b91c9fefadbe4fb82d5a87b8
Joel Gaspar <joel.e.gaspar@gmail.com>
2021-01-30 13:24:42 -0700

    integrating api with web view

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import joblib
 import numpy as np
 
 from flask import Flask
-from flask import jsonify
-
+from flask import jsonify, request
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-
+CORS(app,supports_credentials=True, resources={r'/.*': {'origins': '*'}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+#cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route('/happiness', methods=['GET'])
 def predict_happiness():
     model = joblib.load('./models/best_model.pkl')
@@ -13,14 +15,32 @@ def predict_happiness():
     prediction = model.predict(X_test.reshape(1,-1))
     return jsonify({'Happiness score prediction' : list(prediction)})
 
-@app.route('/heart', methods=['GET'])
+@app.route('/heart', methods=['GET', 'POST'])
+@cross_origin(allow_headers=['Content-Type' ], supports_credentials=True)
 def predict_heart_disease():
-    heart_disease = 'No'
-    model = joblib.load('./models/best_model_score_0.9678_heartGradientClass.pkl')
-    X_test = np.array([71,0,0,112,149,0,1,125,0,1.6,1,0,2])
-    prediction = model.predict(X_test.reshape(1,-1))
-    if prediction[0] == 1:
-        heart_disease = 'Yes'
-    return 'Have a heart disease?: ' + heart_disease
+    if request.method == 'POST':
+        age = request.form.get('age')
+        sex = request.form.get('sex')
+        cp = request.form.get('cp')
+        restbps = request.form.get('restbps')
+        chol = request.form.get('chol')
+        fbs = request.form.get('fbs')
+        restecg = request.form.get('restecg')
+        thalach = request.form.get('thalach')
+        exang = request.form.get('exang')
+        oldpeak = request.form.get('oldpeak')
+        slope = request.form.get('slope')
+        ca = request.form.get('ca')
+        thal = request.form.get('thal')
+        heart_disease = 'No'
+        model = joblib.load('./models/best_model_score_0.9678_heartGradientClass.pkl')
+        X_test = np.array([int(age),int(sex),int(cp),int(restbps),int(chol),int(fbs),int(restecg),int(thalach),int(exang),float(oldpeak),int(slope),int(ca),int(thal)])
+        prediction = model.predict(X_test.reshape(1,-1))
+        if prediction[0] == 1:
+            heart_disease = 'Yes'
+            return jsonify({'Heart disease' : 1})
+        else:
+            return jsonify({'Heart disease' : 0})
+        
 if __name__ == "__main__":
     app.run()
